bffc44555be49352022b8fc4823955b59eeeb41e
WarFly <sahupinku05@gmail.com>
2017-10-28 22:00:09 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/lottery/in/matka/MainActivity.java

--- app/src/main/java/lottery/in/matka/MainActivity.java
+++ app/src/main/java/lottery/in/matka/MainActivity.java
@@ -2,15 +2,28 @@ package lottery.in.matka;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.gson.JsonObject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import lottery.in.matka.fragments.DashboardFragment;
+import lottery.in.matka.retrofit.ApiService;
+import lottery.in.matka.retrofit.ServiceInterface;
+import lottery.in.matka.retrofit.ServiceResponse;
 import lottery.in.matka.utils.Util;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class MainActivity extends BaseAcitivity {
+public class MainActivity extends FirebaseAcitivity {
     @Bind(R.id.toolbar)
     public Toolbar toolbar;
+
+    private ServiceInterface apiServiceFirebase;
+    private Call<ServiceResponse> call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,12 @@ public class MainActivity extends BaseAcitivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
+        //generate firebase token
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.e("TAG", "FCM Token: " + token);
+
+        apiServiceFirebase = ApiService.getRetrofitClient().create(ServiceInterface.class);
+        sendPushNotification(token);
 
     }
 
@@ -28,5 +47,33 @@ public class MainActivity extends BaseAcitivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new DashboardFragment())
                 .addToBackStack(null)
                 .commit();
+    }
+
+    private void sendPushNotification(String token) {
+        //make json
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("to", token);
+        jsonObject.addProperty("priority", "high");
+        JsonObject jsonObjectInner = new JsonObject();
+        jsonObjectInner.addProperty("msg", "Hello Firebase");
+        jsonObjectInner.addProperty("type", "7");
+        jsonObjectInner.addProperty("recruiterId", "4");
+        jsonObject.add("data",jsonObjectInner);
+        Log.d("TAG", "jsonObject: " + jsonObject);
+
+
+
+        call = apiServiceFirebase.sendNotification(jsonObject);
+        call.enqueue(new Callback<ServiceResponse>() {
+            @Override
+            public void onResponse(Call<ServiceResponse> call, Response<ServiceResponse> response) {
+                Log.d("TAG", "send notofication response is- : " + response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<ServiceResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
